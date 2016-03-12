b3fde6398ac94e46d2380f842f3663036561ad07
andela-sjames <samuel.james@andela.com>
2016-03-12 23:20:08 +0100

    add onsubmit and onchange event to UI.

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,12 +1,29 @@
 var React = require('react')
 
 var Search = React.createClass({
+
+    getInitialState: function() {
+        return {
+            city: ''
+        }
+    },
+
+    onUpdateCity: function (e) {
+        this.setState({
+            city: e.target.value
+        })
+    },
+
+    onSubmitCity: function () {
+        console.log(this.state.city)
+    },
+
     render: function(){
         var direction = this.props.flexDirection
         return (
             <div className="search" style={{flexDirection: direction ? direction : "row"}}>
-                <input style={{marginBottom: direction ? "10px" : "0"}} type="text"  placeholder="55 Moleye street"/>
-                <button className="btn btn-success">Get Weather</button>
+                <input style={{marginBottom: direction ? "10px" : "0"}} type="text" onChange={this.onUpdateCity} value={this.state.city} placeholder="55 Moleye street"/>
+                <button className="btn btn-success" onClick={this.onSubmitCity}>Get Weather</button>
             </div>
 
         )
